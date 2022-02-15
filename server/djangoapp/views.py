01da836cc77bb23b8bc61d25ee7bf55748483e01
Fakhreddine Yousfi <Fakhriyo@hotmail.fr>
2022-02-15 16:32:40 +0000

    changes made

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -28,15 +28,41 @@ logger = logging.getLogger(__name__)
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    if request.method == "Post":
+        #get username and password from request.post dictionary
+        username = request.POST['username']
+        password = request.POST['password']
+        #Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If user is valid cll login method to login current user
+            login(request,user)
+            return render(request,'djangoapp/index.html',context)
+        else:
+            # If not, return to login page again 
+            return render(request,'djangoapp/index.html',context)
+    else:
+        return render(request, 'djangoapp/index.html')
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
-
+def logout_request(request):
+    #Get the user object based on session id in request
+    print("log out the user `{}`".format(request.user.username))
+    # Logout user in the request
+    logout(request)
+    # Redirect user back to the home page 
+    return redirect ('djangoapp/index.html')
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
-
+def registration_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html', context)
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
