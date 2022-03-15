12d391bd0041678a99cae28d391f85ca61416945
Fakhreddine85 <64703898+Fakhreddine85@users.noreply.github.com>
2022-03-15 16:01:35 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,15 +3,31 @@ from django.utils.timezone import now
 
 
 # Create your models here.
-
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=30)
+    Description = models.CharField(max_length=100)
+    def __str__(self):
+        return self.Name
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    carMake = models.ForeignKey(CarMake,on_delete=models.CASCADE)
+    Name = models.CharField(max_length=30)
+    Dealer_id = models.IntegerField()
+    class TypeChoices(models.TextChoices):
+        SEDAN = '1', "Sedan"
+        SUV = '2', "Suv"
+        WAGON = '3', "Wagon"
+    Type = models.CharField(max_length=30,choices=TypeChoices.choices,default=TypeChoices.SEDAN) 
+    Year = models.DateField()
+    def __str__(self):
+        return self.Name
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# <HINT> Create a Car Model model `class CarModel(models.Model):
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
@@ -22,6 +38,7 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+#class CarDealer (models.Model):
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+#class DealerReview (models.Model):
